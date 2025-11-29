ded489a38c87f65080c5e36ed996913e269480ec
Steven <admin@qiangmou.ren>
2025-11-30 02:30:16 +0800

    chore: update test script

--- test/test.ps1
+++ test/test.ps1
@@ -1,23 +1,23 @@
-﻿Write-Host "正在编译 main.py..."
+﻿Write-Host "Compiling main.py..."
 nuitka .\main.py --mode=onefile
 
 if ($LASTEXITCODE -ne 0) {
-    Write-Host "Nuitka编译失败！"
+    Write-Host "Nuitka compilation failed!"
     exit
 }
 
-Write-Host "正在提取可执行文件..."
+Write-Host "Extracting executable..."
 .\nuitka-extractor.exe .\main.exe
 
 if ($LASTEXITCODE -ne 0) {
-    Write-Host "提取器执行失败！"
+    Write-Host "Extractor execution failed!"
     exit
 }
 
-Write-Host "正在复制文件..."
+Write-Host "Copying files..."
 Copy-Item ..\boot.exe main.exe_extracted\boot.exe
 Copy-Item .\main.exe main.exe_extracted\main.exe
 
 Set-Location main.exe_extracted
-Write-Host "启动应用程序..."
+Write-Host "Launching application..."
 .\boot.exe main.exe
